2583d9ee1b53d740ff58c2beb11f1115d5fb7746
hanon11 <71835531+hanon11@users.noreply.github.com>
2021-04-16 11:34:29 +0200

    Update practica2.sql

--- practicas/practica2.sql
+++ practicas/practica2.sql
@@ -1,53 +1,53 @@
 /*Ejercicio 21*/
-/*select distinct tda_pob
+select distinct tda_pob
 from tiendas
-order by tda_pob*/
+order by tda_pob
 
 /*Ejercicio 22*/
-/*select distinct clt_pob, clt_pais
-from clientes*/
+select distinct clt_pob, clt_pais
+from clientes
 
 /*Ejercicio 23*/
-/*select art_nom 'Name', art_col 'Colour', art_pv 'Selling price'
+select art_nom 'Name', art_col 'Colour', art_pv 'Selling price'
 from articulos
 where art_prv=2
-order by art_pv DESC, art_nom*/
+order by art_pv DESC, art_nom
 
 /*Ejercicio 24*/
-/*select  vnt_tda 'Shop', vnt_art 'Item',vnt_cant 'Quantity', vnt_fch 'Date of purchase'
+select  vnt_tda 'Shop', vnt_art 'Item',vnt_cant 'Quantity', vnt_fch 'Date of purchase'
 from ventas
-where vnt_clt=1 AND vnt_art between 12 and 20*/
+where vnt_clt=1 AND vnt_art between 12 and 20
 
 /*Ejercicio 25*/
-/*select tda_ger 'Manager', tda_pob 'Town'
+select tda_ger 'Manager', tda_pob 'Town'
 from tiendas
 where tda_pob NOT IN ('París', 'Lyon') AND tda_pob not LIKE 'Madrid%'
-order by tda_ger, tda_pob */
+order by tda_ger, tda_pob 
 
 
 /*Ejercicio 26*/
-/*select art_nom
+select art_nom
 from articulos
 where art_peso AND art_col is not null AND (art_nom not LIKE 'b%' OR art_nom not LIKE 'i%')
-order by art_pc DESC*/
+order by art_pc DESC
 
 /*Ejercicio 27*/
-/*select vnt_fch 'Date'
+select vnt_fch 'Date'
 from ventas
 where vnt_art = 9
-order by vnt_fch*/
+order by vnt_fch
 
 
 /*Ejercicio 28*/
-/*select prv_num 'Code number', prv_nom 'Name of supplier'
+select prv_num 'Code number', prv_nom 'Name of supplier'
 from proveedores
-where prv_nom LIKE '%ca%' AND prv_nom NOT LIKE '%nic'*/
+where prv_nom LIKE '%ca%' AND prv_nom NOT LIKE '%nic'
 
 
 /*Ejercicio 29*/
-/*select *
+select *
 from tiendas
-where tda_ger LIKE '%Juan Carlos%'*/
+where tda_ger LIKE '%Juan Carlos%'
 
 /*Ejercicio 30*/
 select clt_nom, clt_apell
